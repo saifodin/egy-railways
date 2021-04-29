6a1cd0687c03aef4a60c72da27156c361a546407
saifodeen <saifodeen.w@gmail.com>
2021-04-29 18:36:38 +0200

    add CheckBoxOrRadio to trainsBetweenStations page

---
+++ src/components/CheckBoxOrRadio/CheckBoxOrRadio.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import css from './CheckBoxOrRadio.module.scss'
+
+
+const CheckBoxOrRadio = (props) => {
+  // <CheckBoxOrRadio isCheckBoxNotRadio={true} wordsInLabel={array} shapeOfStyles="basic" />
+  // or
+  // <CheckBoxOrRadio isCheckBoxNotRadio={false} wordsInLabel={array} nameOfRadio="gender" shapeOfStyles="basic" />
+
+
+  //#region generate <li> with dynamic data, with radio or checkbox 
+  let typeName = "radio"
+  let radioName = props.nameOfRadio
+  let isCheckbox = "";
+
+  if (props.isCheckBoxNotRadio) {
+    typeName = "checkbox"
+    isCheckbox = css.checkbox
+  }
+
+  let items = []
+  for (let i = 0; i < props.wordsInLabel.length; i++) {
+
+    let word = props.wordsInLabel[i]
+
+    // in radio all name are equal, in checkBox name is unique
+    if (props.isCheckBoxNotRadio) {
+      radioName = word;
+    }
+
+    items.push(
+      <li>
+        {/*input => disable none */}
+        <input type={typeName} id={word} name={radioName} />
+        {/*label => the word */}
+        <label className={isCheckbox} for={word}>{word}</label>
+        {/*div => the circle or rectangle  */}
+        <div className={isCheckbox}></div>
+      </li>
+    );
+  }
+  //#endregion
+
+  //#region change some styles in different pages
+  let extraStyle = ""
+  if (props.shapeOfStyles === "basic") {
+    extraStyle = css.basicStyles
+  }
+  
+  //#endregion
+  return (
+    <div className={`${css.CheckBoxOrRadioContainer} ${extraStyle}`}>
+      <ul>
+        {items}
+        {/*
+        <li>
+          <input type={typeName} id={word} name={radioName} />
+          <label className={isCheckbox} for={word}>{word}</label>
+          <div className={css.isCheckbox}></div>
+        </li> 
+      */}
+      </ul>
+    </div>
+  );
+}
+
+export default CheckBoxOrRadio;
--- src/pages/TrainsBetweenStations/TrainsBetweenStations.js
+++ src/pages/TrainsBetweenStations/TrainsBetweenStations.js
@@ -2,17 +2,88 @@ import React from 'react';
 import css from './TrainsBetweenStations.module.scss'
 import Navbar from '../../components/Navbar/Navbar'
 import SearchBar from '../../components/SearchBar/SearchBar';
+import CheckBoxOrRadio from '../../components/CheckBoxOrRadio/CheckBoxOrRadio'
+import pngMorning from '../../assets/imgs/time/morning.png'
+import pngAfternoon from '../../assets/imgs/time/afternoon.png'
+import pngEvening from '../../assets/imgs/time/evening.png'
+import pngNight from '../../assets/imgs/time/night.png'
 
-const TrainsBetweenStations = props => (
-  <div className={css.TrainsBetweenStations}>
-    <div className={css.upperPart}>
-      <Navbar />
-      <div className={css.line}></div>
-      <div className={css.SearchBarContainer}>
-        <SearchBar />
+
+const TrainsBetweenStations = props => {
+  return (
+    <div className={css.TrainsBetweenStations}>
+      <div className={css.upperPart}>
+        <Navbar />
+        <div className={css.line}></div>
+        <div className={css.SearchBarContainer}>
+          <SearchBar />
+        </div>
+      </div>
+      <div className={css.mainPart}>
+        <div className={css.filtersContainer}>
+          <div className={`${css.filters} container`}>
+
+            <div className={css.class}>
+              <h4>Class</h4>
+              <div className={css.checkBoxOrRadioContainer}>
+                <CheckBoxOrRadio shapeOfStyles="basic" isCheckBoxNotRadio={true} wordsInLabel={['1A', '2A', '3A']} nameOfRadio="gender" />
+              </div>
+            </div>
+
+            <div className={css.departureTime}>
+              <h4>Class</h4>
+              <div className={css.timesContainer}>
+
+                <div>
+                  <input type="checkbox" id="morning" name="morning" />
+                  <label for="morning">
+                    <div class={css.innerSection}>
+                      <img alt="morning" src={pngMorning} />
+                      <p>05 AM - 11 AM</p>
+                    </div>
+                  </label>
+                </div>
+
+                <div>
+                  <input type="checkbox" id="afternoon" name="afternoon" />
+                  <label for="afternoon">
+                    <div class={css.innerSection}>
+                      <img alt="afternoon" src={pngAfternoon} />
+                      <p>05 AM - 11 AM</p>
+                    </div>
+                  </label>
+                </div>
+
+                <div>
+                  <input type="checkbox" id="evening" name="evening" />
+                  <label for="evening">
+                    <div class={css.innerSection}>
+                      <img alt="evening" src={pngEvening} />
+                      <p>05 AM - 11 AM</p>
+                    </div>
+                  </label>
+                </div>
+
+                <div>
+                  <input type="checkbox" id="night" name="night" />
+                  <label for="night">
+                    <div class={css.innerSection}>
+                      <img alt="night" src={pngNight} />
+                      <p>05 AM - 11 AM</p>
+                    </div>
+                  </label>
+                </div>
+
+              </div>
+            </div>
+
+            <div>Sort by</div>
+
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  )
+};
 
 export default TrainsBetweenStations;

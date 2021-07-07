35ed02729ad0cb8e152d8d71c9672c36c690c09e
saifodeen <saifodeen.w@gmail.com>
2021-07-08 00:18:17 +0200

    add smart validation to AuhtComponent

--- src/components/AuthComponent/AuthComponent.js
+++ src/components/AuthComponent/AuthComponent.js
@@ -2,21 +2,88 @@ import React, { useState, Fragment } from 'react';
 import coverWalking from '../../assets/imgs/omioCovers/cover-walking.svg'
 import google from '../../assets/imgs/otherSvg/google.svg'
 import './AuthComponent.scss'
+import { emailValidator } from '../../shared/utility'
 
 const AuthComponent = props => {
 
   const [isSignIn, setIsSignIn] = useState(true)
 
 
+  //#region validation inputs, then add message error
+  const validationErrorBlank = <p className="validationError">This field can't be blank</p>
+  const validationErrorEmailNoValid = <p className="validationError">Invalid email format</p>
+  const validationErrorPassNotValid = <p className="validationError">Must contain at least 8 characters</p>
 
-  let validationError = (
-    <p className="validationError">
-      This field can't be blank
-    </p>
-    // Invalid email format.
-    // Must contain at least 8 characters
+  //* signInEmail
+  const [signInEmail, setSignInEmail] = useState(null)
+  const [isSignInEmailAccess, setIsSignInEmailAccess] = useState(false)
+  let signInEmailError = null
+  if (isSignInEmailAccess) {
+    signInEmailError = !signInEmail ? validationErrorBlank : (!emailValidator(signInEmail) ? validationErrorEmailNoValid : null)
+  }
 
-  )
+  //* signInPass
+  const [signInPass, setSignInPass] = useState(null)
+  const [isSignInPassAccess, setIsSignInPassAccess] = useState(false)
+  let signInPassError = null
+  if (isSignInPassAccess) {
+    signInPassError = !signInPass ? validationErrorBlank : (signInPass.length < 8 ? validationErrorPassNotValid : null)
+  }
+
+  //* signUpFname
+  const [signUpFname, setSignUpFname] = useState(null)
+  const [signUpFnameAccess, setSignUpFnameAccess] = useState(false)
+  let signUpFnameError = null
+  if (signUpFnameAccess) {
+    signUpFnameError = !signUpFname ? validationErrorBlank : null
+  }
+
+  //* signUpLname
+  const [signUpLname, setSignUpLname] = useState(null)
+  const [signUpLnameAccess, setSignUpLnameAccess] = useState(false)
+  let signUpLnameError = null
+  if (signUpLnameAccess) {
+    signUpLnameError = !signUpLname ? validationErrorBlank : null
+  }
+
+  //* signUpEmail
+  const [signUpEmail, setSignUpEmail] = useState(null)
+  const [signUpEmailAccess, setSignUpEmailAccess] = useState(false)
+  let signUpEmailError = null
+  if (signUpEmailAccess) {
+    signUpEmailError = !signUpEmail ? validationErrorBlank : (!emailValidator(signUpEmail) ? validationErrorEmailNoValid : null)
+  }
+
+  //* signUpPass
+  const [signUpPass, setSignUpPass] = useState(null)
+  const [isSignUpPassAccess, setIsSignUpPassAccess] = useState(false)
+  let signUpPassError = null
+  if (isSignUpPassAccess) {
+    signUpPassError = !signUpPass ? validationErrorBlank : (signUpPass.length < 8 ? validationErrorPassNotValid : null)
+  }
+  //#endregion
+
+  const submitSignIn = _ => {
+    // if all inputs is not null && no errors, why? (before focus on any inputs no errors yet, so we must check if inputs is null or not)
+    if (signInEmail && signInPass && !signInEmailError && !signInPassError) {
+      //* push to server
+    } else {
+      setIsSignInEmailAccess(true)
+      setIsSignInPassAccess(true)
+    }
+  }
+
+  const submitSignUp = _ => {
+    if (signUpFname && signUpLname && signUpEmail && signUpPass && !signInEmailError && !signInPassError) {
+      //* push to server
+    } else {
+      setSignUpFnameAccess(true)
+      setSignUpLnameAccess(true)
+      setSignUpEmailAccess(true)
+      setIsSignUpPassAccess(true)
+    }
+  }
+
 
   return (
     <Fragment>
@@ -46,15 +113,15 @@ const AuthComponent = props => {
               <div className="withEmail">
                 <div className="email">
                   <label htmlFor="email">Your email address</label>
-                  <input type="email" id="email" name="email" />
-                  {validationError}
+                  <input className={signInEmailError ? "inputError" : null} type="email" id="email" name="email" onBlur={_ => setIsSignInEmailAccess(true)} onChange={e => setSignInEmail(e.target.value.trim())} />
+                  {signInEmailError}
                 </div>
                 <div className="password">
                   <label htmlFor="password">Your password</label>
-                  <input type="password" id="password" name="password" />
-                  {validationError}
+                  <input className={signInPassError ? "inputError" : null} type="password" id="password" name="password" onBlur={_ => setIsSignInPassAccess(true)} onChange={e => setSignInPass(e.target.value)} />
+                  {signInPassError}
                 </div>
-                <button className="submitButton">Sign in</button>
+                <button className="submitButton" onClick={submitSignIn}>Sign in</button>
               </div>
               <div className="toAnotherSign">
                 <p>Don't have an account? <span onClick={_ => setIsSignIn(false)}>Sign up</span></p>
@@ -78,26 +145,26 @@ const AuthComponent = props => {
                 <div className="name">
                   <div className="firstName">
                     <label htmlFor="fname">First name</label>
-                    <input type="text" id="fname" name="fname" />
-                    {validationError}
+                    <input className={signUpFnameError ? "inputError" : null} onBlur={_ => setSignUpFnameAccess(true)} onChange={e => setSignUpFname(e.target.value.trim())} type="text" id="fname" name="fname" />
+                    {signUpFnameError}
                   </div>
                   <div className="lastName">
                     <label htmlFor="lname">Last name</label>
-                    <input type="text" id="lname" name="lname" />
-                    {validationError}
+                    <input className={signUpLnameError ? "inputError" : null} onBlur={_ => setSignUpLnameAccess(true)} onChange={e => setSignUpLname(e.target.value.trim())} type="text" id="lname" name="lname" />
+                    {signUpLnameError}
                   </div>
                 </div>
                 <div className="email">
                   <label htmlFor="email">Your email address</label>
-                  <input type="email" id="email" name="email" />
-                  {validationError}
+                  <input className={signUpEmailError ? "inputError" : null} onBlur={_ => setSignUpEmailAccess(true)} onChange={e => setSignUpEmail(e.target.value.trim())} type="email" id="email" name="email" />
+                  {signUpEmailError}
                 </div>
                 <div className="password">
                   <label htmlFor="password">Your password</label>
-                  <input type="password" id="password" name="password" />
-                  {validationError}
+                  <input className={signUpPassError ? "inputError" : null} onBlur={_ => setIsSignUpPassAccess(true)} onChange={e => setSignUpPass(e.target.value)} type="password" id="password" name="password" />
+                  {signUpPassError}
                 </div>
-                <button className="submitButton">Sign up</button>
+                <button className="submitButton" onClick={submitSignUp}>Sign up</button>
               </div>
               <div className="toAnotherSign">
                 <p>Already have an account? <span onClick={_ => setIsSignIn(true)}>Sign in</span></p>
--- src/shared/utility.js
+++ src/shared/utility.js
@@ -458,3 +458,9 @@ export const KnowGov = (station) => {
 export const refreshPage = _ => {
   window.location.reload();
 }
+
+
+export const emailValidator = email => {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email));
+}
